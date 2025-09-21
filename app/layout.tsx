30f54ac1c3fa721f75c81b2906fc496f58a6609b
Asya <shamalo.n.75@gmail.com>
2025-09-21 19:50:59 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import { Roboto } from 'next/font/google'
+
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+});
+
+
+export const metadata: Metadata = {
+  title: "NoteHub",
+  description: "An application for note-taking and organization",
+  openGraph:
+  {
+    title: "NoteHub",
+    description: "An application for note-taking and organization",
+    url: "https://notehub.example.com",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub - An application for note-taking and organization",
+      }
+    ]
+  }
+};
+
+export default function RootLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable}`}>
+        <TanStackProvider>
+          <Header />
+          {children}
+          {modal}
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+const loading = () =>{
+    return (
+    <p>Loading, please wait...</p>
+    )
+}
+
+export default loading;
